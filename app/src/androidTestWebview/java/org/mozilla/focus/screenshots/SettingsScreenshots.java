d95af2a81c1918cfe4eb840bd99950184ff0e027
No-Jun Park <npark@mozilla.com>
2017-12-02 11:46:04 -0500

    "custom URLs" locale specify

--- app/src/androidTestWebview/java/org/mozilla/focus/screenshots/SettingsScreenshots.java
+++ app/src/androidTestWebview/java/org/mozilla/focus/screenshots/SettingsScreenshots.java
@@ -156,16 +156,10 @@ public class SettingsScreenshots extends ScreenshotTest {
         onView(withText(getString(R.string.preference_autocomplete_custom_summary)))
                 .perform(click());
         /* Add custom URL */
-        final String key = InstrumentationRegistry
-                .getTargetContext()
-                .getString(R.string.pref_key_screen_custom_domains);
-        onData(withKey(key))
+        onView(allOf(withText(getString(R.string.preference_autocomplete_subitem_customlist)),withParentIndex(0)))
                 .perform(click());
-<<<<<<< HEAD:app/src/androidTest/java/org/mozilla/focus/screenshots/SettingsScreenshots.java
 
-=======
         device.waitForIdle();
->>>>>>> reset after each test:app/src/androidTest/java/org/mozilla/focus/activity/screenshots/SettingsScreenshots.java
         final String addCustomURLAction = getString(R.string.preference_autocomplete_action_add);
         onView(withText(addCustomURLAction))
                 .check(matches(isDisplayed()));
